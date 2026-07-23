2c1ea157fae67c39145ab82cf9d27897881d6f9f
Enrique Farias <Enrique.Farias@acronis.com>
2026-07-22 19:03:55 -0600

    corrections on base

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, jsonify
 from services.timezone_service import get_city_times
-
+ 
 app = Flask(__name__)
-
+ 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+return render_template("index.html")
+ 
 @app.route("/api/times")
 def times():
-    return jsonify(get_city_times())
-
+return jsonify(get_city_times())
+ 
 if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
